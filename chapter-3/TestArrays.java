85b6933e0d332c0e868149f874168f735d52b2a4
Bennett <bchidoro9@gmail.com>
2017-09-11 20:56:47 -0400

    :school_satchel: Add chapter 3 exercises

---
+++ chapter-3/TestArrays.java
@@ -0,0 +1,35 @@
+class TestArrays {
+  public static void main (String[] args) {
+    String [] islands = new String[4];
+    int [] index = new int[4];
+    int y = 0;
+    int ref;
+
+    islands[0] = "Bermuda";
+    islands[1] = "Fiji";
+    islands[2] = "Azores";
+    islands[3] = "Cozumel";
+
+    index[0] = 1;
+    index[1] = 3;
+    index[2] = 0;
+    index[3] = 2;
+
+
+    while (y < 4) {
+      ref = index[y];
+      y = y + 1;
+      System.out.print("island = ");
+      System.out.println(islands[ref]);
+    }
+  }
+}
+
+/*
+
+island = Fiji
+island = Cozumel
+island = Bermuda
+island = Azores
+
+*/
