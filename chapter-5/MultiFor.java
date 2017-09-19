257ead87bda41818bb4e357833563dfc6339f5d0
Bennett <bchidoro9@gmail.com>
2017-09-18 21:34:30 -0400

    :school_satchel: Add chapter 5 exercises

---
+++ chapter-5/MultiFor.java
@@ -0,0 +1,24 @@
+class MultiFor {
+  public static void main(String[] args) {
+    for (int x = 0; x < 4; x++) {
+      for (int y = 4; y > 2; y-- ) {
+          System.out.println(x + " ");
+          if (x == 1) {
+          x++;
+        }
+      }
+    }
+  }
+}
+
+
+/*
+
+0 4
+0 3
+1 4
+1 3
+3 4
+3 3
+
+*/
